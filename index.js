0c6a42015cb8b25d66e550cdd1dbd93416ff7513
synchronous-environment-3669 <kyu22@buffalo.edu>
2018-10-10 19:24:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
 var conversation = `${topic}`;
-https://github.com/kenneth-yu/javascript-strings-lab-js-apply-000-js-apply-000
